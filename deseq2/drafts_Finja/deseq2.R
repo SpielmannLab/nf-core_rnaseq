9aa285fc9486bc8507956beff3952e08b90aa3cd
Finja Sygo <145127464+finjasygo@users.noreply.github.com>
2024-03-08 14:37:35 +0100

    updating the files for optimization
    Optimization and correction of files, including the deletion of unnecessary information.
    And change of the parameter name 'pVal' to 'alpha'.

--- deseq2/drafts_Finja/deseq2.R
+++ deseq2/drafts_Finja/deseq2.R
@@ -1,6 +1,12 @@
 suppressPackageStartupMessages(library(rmarkdown))
 suppressPackageStartupMessages(library(yaml))
 
-params <- read_yaml("params_deseq2.yaml")
+yaml_file <- "params_deseq2.yaml"
+rmd_file <- "deseq2.rmd"
+
+params <- read_yaml(yaml_file)
 list2env(params, envir = environment())
-render("deseq2.rmd", output_file=sprintf("%s_vs_%s_deseq2",alternate_condition,reference_condition), output_dir = outdir)
+outdir_folder <- sprintf("%s_vs_%s",alternate_condition,reference_condition)
+dir.create(outdir_folder)
+outdir <- sprintf("%s/%s",out_dir,outdir_folder)
+render(rmd_file, output_file=sprintf("report_%s_vs_%s_deseq2",alternate_condition,reference_condition), output_dir = outdir)
