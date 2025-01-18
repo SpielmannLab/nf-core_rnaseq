e6d3a030c22c214972b1e0b43b1452b96ceb7f07
varunkas <varun.sreenivasan@outlook.com>
2025-01-19 00:24:14 +0100

    Add fgsea to DESeq2 analysis. Fix logFC direction
    Add fgsea to the analysis
    Fixed the LogFC direction

--- de_analysis/deseq2/deseq2.R
+++ de_analysis/deseq2/deseq2.R
@@ -7,6 +7,6 @@ rmd_file <- "deseq2.rmd"
 params <- read_yaml(yaml_file)
 list2env(params, envir = environment())
 outdir_folder <- sprintf("%s_vs_%s",alternate_condition,reference_condition)
-dir.create(outdir_folder)
 outdir <- sprintf("%s/%s",out_dir,outdir_folder)
+dir.create(outdir)
 render(rmd_file, output_file=sprintf("report_%s_vs_%s_deseq2",alternate_condition,reference_condition), output_dir = outdir)
